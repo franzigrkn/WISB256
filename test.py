d60530c7459ebca37ec84647bd5e5a605c35a2f3
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-04-28 09:42:44 +0000

    naam veranderd

--- test.py
+++ test.py
@@ -8,6 +8,9 @@ line3='goodbye'
 document.write(line3)
 document.close()
 
+import time
+T1 = time.perf_counter()
+
 fin = open('words.txt')
 
 t=[]
@@ -16,6 +19,23 @@ for line in fin:
     print(t)
 
 fin.close()
+T2 = time.perf_counter()
+fin = open('words.txt')
+
+t=[]
+for line in fin:
+    t.append(line[0])
+    print(t)
+
+fin.close()
+T3 = time.perf_counter()
+
+print('Time required', T2 - T1, 'sec.')
+print('Time required', T3 - T2, 'sec.')
+
+
+
+
 
 
 
