b268e23d3f4da96f80e296463db1bdf4681e7356
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-04-29 16:18:37 +0000

    inleveropgave test

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,3 +1,5 @@
+import time
+T1=time.perf_counter()
 
 def prime(n):
     getallen = list(range(2,n+1, 1))
@@ -5,10 +7,34 @@ def prime(n):
         for x in getallen:
             if x%deler==0 and x!=deler:
                 getallen.remove(x)
-                print(getallen)
-            
-       
-prime(21)
+    print(getallen)
+   
+
+prime(10000)
+
+
+T2=time.perf_counter()
+print('Time required', T2-T1, 'sec.')
+print('Found x Prime numbers smaller than n in', T2-T1 , 'sec.')
+
+
+fin = open( 'prime.dat', 'w')
+print(fin)
+line1 = 'hallo'
+fin.write(line1)
+line2 = 'hallo'
+fin.write(line2)
+fin.close()
+
+
+    
+    
+
+
+
+
+
+
         
             
             
