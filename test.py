a97e4485c6f634efd40ea5e900072c59b8f03b7a
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-05-05 11:03:16 +0000

    Opgaven uit boek

--- test.py
+++ test.py
@@ -1,39 +1,9 @@
-document = open('words.txt', 'w')
-print (document)
-line1= 'Hello \n'
-document.write(line1)
-line2= 'world \n'
-document.write(line2)
-line3='goodbye'
-document.write(line3)
-document.close()
+word='hello'
 
-import time
-T1 = time.perf_counter()
+middle=str(word[1:-1])
 
-fin = open('words.txt')
-
-t=[]
-for line in fin:
-    t=t+[line[0]]
-    print(t)
-
-fin.close()
-T2 = time.perf_counter()
-fin = open('words.txt')
-
-t=[]
-for line in fin:
-    t.append(line[0])
-    print(t)
-
-fin.close()
-T3 = time.perf_counter()
-
-print('Time required', T2 - T1, 'sec.')
-print('Time required', T3 - T2, 'sec.')
-
-print(int(3.2))
+print(middle)
+print(type(middle))
 
 
 
