c5ff600e9a1cdf427f597bc68d0f715923bf9ddd
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-05-07 14:37:39 +0000

    Inlever3

--- Opdracht3/estimate_pi.py
+++ Opdracht3/estimate_pi.py
@@ -1,21 +1,21 @@
 import random
 import math
+import sys
+seed= sys.argv[3]
+random.seed(int(seed))
 
-x = random.random() # uniform in [0,1]
-y = random.random()
-z = random.vonmisesvariate(0,0) #uniform in [0,2*pi]
 
 def drop_needle(L):
-    x_0=x
-    y_0=y
-    a=z
+    x_0=random.random() # uniform in [0,1]
+    #y_0=random.random()
+    a=random.vonmisesvariate(0,0) #uniform in [0,2*pi]
     x_1= x_0 + L*math.cos(a)
-    y_1 = y_0 + L*math.sin(a)
-    print(x_0)
-    print(y_0)
-    print(a)
-    print(x_1)
-    print(y_1)
+    #y_1 = y_0 + L*math.sin(a)
+    #print(x_0)
+    #print(y_0)
+    #print(a)
+    #print(x_1)
+    #print(y_1)
     if  x_1 >= 1:
         return True
     elif x_1 <= 0:
@@ -24,4 +24,43 @@ def drop_needle(L):
         return False
         
 
-print(drop_needle(0.7))
+
+
+
+def hits_needle():
+    if len(sys.argv) <3:
+        print('Use: estimate_pi.py N L')
+        return
+    
+    N=int(sys.argv[1])
+    L=float(sys.argv[2])
+
+    
+    if L > 1:
+        print('AssertionError: L should be smaller than 1')
+        return
+    
+    count=0
+    for i in range(1,N+1):
+        if drop_needle(L) == True:
+            count=count+1
+    print(str(count) + ' hits in ' + str(N) + ' tries')
+    pi= (2*N*L)/count
+    print('Pi = ' + str(pi))
+    return
+            
+
+hits_needle()
+            
+    
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+
--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-import random
-
-x = random.random() # uniform in [0,1]
-y= random.random()
-a = random.vonmisesvariate(0,0) #uniform in [0,2*pi]
-
-for i in range(5):
-    print(x)
-    print(y)
+print(range(1,5))
 
 
 
