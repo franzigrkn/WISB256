c5a1bc8357ab41649a5802059cdaf99e3cd6b363
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-05-12 09:52:59 +0000

    verbetering Opdracht4

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,14 +1,12 @@
 def findRoot(f,a,b,epsilon):
+    if f(a)*f(b)>0:
+        return 'kaas'
     if f(a)==0: #if a is root, return a
         return a
     elif f(b)==0: # if b is root, return b
         return b
-    
-    m=(b-a)/2
-    print(m)
-    if abs(b-a) < epsilon:
-        return m
-    elif f(m)==0:
+    m=(b+a)/2
+    if abs(b-a) <= epsilon:
         return m
     elif f(m)*f(b) > 0:
         return findRoot(f,a,m,epsilon)
@@ -16,15 +14,6 @@ def findRoot(f,a,b,epsilon):
         return findRoot(f,m,b,epsilon)
     
 
-    
-        
-
-def f(x):
-    x-1
-    return x-1
-
-
-print(findRoot(f,0,2,0.1))
 
         
     
--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-print(abs(-7))
+import bisection
+
+def f(x):
+    x-1
+    return x-1
+    
+root = bisection.findRoot(f,0,1,0.1)
+print(root)
 
 
 
