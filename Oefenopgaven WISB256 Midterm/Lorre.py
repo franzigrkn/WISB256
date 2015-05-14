2fa1cfaa976876adf426b65c6e8c1de167e66b41
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-05-14 15:25:40 +0000

    eerste oefenopgave

--- Oefenopgaven WISB256 Midterm/Lorre.py
+++ Oefenopgaven WISB256 Midterm/Lorre.py
@@ -1,1 +1,15 @@
-input()
+number=input()
+#print(number)
+getallen= list(range(1, int(number)+1))
+#print(getallen)
+for i in range(1, int(number)+1):
+    uitspraak_i = input()
+    count=0
+    for letter in uitspraak_i:
+        if letter == ' ':
+            count=count+1
+    #print(count+1)
+    if count +1 > 4:
+        print('Crackers!')
+    else:
+        print(uitspraak_i + ' krAh!')
--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-import bisection
-
-def f(x):
-    x-1
-    return x-1
-    
-root = bisection.findAllRoot(f,0,1,0.1)
-print(root)
-
+count=0
+word=input()
+for letter in word:
+    if letter == ' ':
+        count=count+1
+print(count)
+        
+            
 
 
 
