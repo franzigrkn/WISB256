4e1c895d885135cd928eaa4e5e11e2791172d26e
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-05-15 17:11:36 +0000

    veranderingen staande_ovatie

--- Oefenopgaven WISB256 Midterm/Staande_Ovatie.py
+++ Oefenopgaven WISB256 Midterm/Staande_Ovatie.py
@@ -8,17 +8,18 @@ list=[]
 for i in range(0, int(maximum)+1):
     list.append(int(schema[i]))
 #print(list)
-#print(sum(list[0:2]))
+
+
 vrienden=[]
 for i in range(1, int(maximum)+1):
-    if sum(list[:i]) >= i:
+    if sum(list[:i]) + sum(vrienden) >= i:
         vrienden.append(0)
-    elif sum(list[:i]) < i and list[i]==0:
+    elif sum(list[:i]) + sum(vrienden) < i and list[i]==0:
         vrienden.append(0)
     elif sum(list[:i])+sum(vrienden) < i and list[i] > 0:
-        x= i-sum(list[:i])
+        x= i-sum(list[:i])-sum(vrienden)
         vrienden.append(x)
-#print(vrienden)
+    #print(vrienden)
 
 answer=sum(vrienden)
 print(answer)
