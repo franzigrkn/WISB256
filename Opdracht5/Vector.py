391ac8f87c80fc2f16e3273d08f0b5edc4dccb67
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-05-26 10:36:08 +0000

    inlever 5

--- Opdracht5/Vector.py
+++ Opdracht5/Vector.py
@@ -1,3 +1,5 @@
+import math
+
 class Vector:
     ''' Klasse voor Vectoren in R^n
     eigenschappen: dimensie, lijst van floats
@@ -7,12 +9,17 @@ class Vector:
     
     __slots__=('dimensie', 'elementen')
     
-    def __init__(self, n, scalar=0):
+    def __init__(self, n, constante=0):
         self.vector=[]
         self.dimensie=n
-        for i in range(n):
-            self.vector.append(scalar)
-        self.elementen=self.vector
+        if type(constante)!=list:
+            for i in range(n):
+                self.vector.append(constante)
+            self.elementen=self.vector
+        if type(constante)==list:
+            for i in range(n):
+                self.vector.append(constante[i])
+            self.elementen=self.vector
             
         
             
@@ -25,6 +32,36 @@ class Vector:
         for i in range(int(self.dimensie)):
             answer= answer + str(self.elementen[i]) + '\n'
         return answer
+        
+    def lincomb(self, other, alpha,beta):
+        lijst=[]
+        for i in range(self.dimensie):
+            lijst.append (alpha*self.elementen[i]+beta*other.elementen[i])
+        return Vector(self.dimensie, lijst)
+        
+    def scalar(self, alpha):
+        lijst=[]
+        for i in range(self.dimensie):
+            lijst.append(alpha*self.elementen[i])
+        return Vector(self.dimensie, lijst)
+        
+    def inner(self, other):
+        answer=0
+        for i in range(self.dimensie):
+            answer=answer + (self.elementen[i]*other.elementen[i])
+            #print(answer)
+        return(answer)
+        
+    def norm(self):
+        answer=0
+        for i in range(self.dimensie):
+            answer= answer + (self.elementen[i]*self.elementen[i])
+        answer=math.sqrt(answer)
+        return answer
+        
+        
+        
+        
     
 
         
--- Opdracht5/test_vector.py
+++ Opdracht5/test_vector.py
@@ -1,6 +1,12 @@
 from Vector import Vector
 
-u_1 = Vector(3)
+u = Vector(3,[1,2,3])
+v = Vector(3, 3.5)
+w=u.lincomb(v,10,1)
+w=w.scalar(2)
+print(w)
+print(w.norm())
+print(w.inner(u))
 
 
-print(u_1)
+
