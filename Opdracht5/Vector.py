afeeb0c78024f4dc8ed00ab9afaa73db2483e4ac
Franziska Gerken <franzi_gerken93@hotmail.de>
2015-05-28 12:44:16 +0000

    inlever5 bonus

--- Opdracht5/Vector.py
+++ Opdracht5/Vector.py
@@ -1,13 +1,13 @@
 import math
 
-class Vector:
+class Vector():
     ''' Klasse voor Vectoren in R^n
     eigenschappen: dimensie, lijst van floats
     methoden : add, scalar_product, inn_product
     
     '''
     
-    __slots__=('dimensie', 'elementen')
+    #__slots__=('dimensie', 'elementen', 'vector')
     
     def __init__(self, n, constante=0):
         self.vector=[]
@@ -30,7 +30,7 @@ class Vector:
         #print(self.elementen[0])
         #print(self.dimensie)
         for i in range(int(self.dimensie)):
-            answer= answer + str(self.elementen[i]) + '\n'
+            answer= answer + str("{0:.6f}".format(self.elementen[i])) + '\n'
         return answer
         
     def lincomb(self, other, alpha,beta):
@@ -58,6 +58,38 @@ class Vector:
             answer= answer + (self.elementen[i]*self.elementen[i])
         answer=math.sqrt(answer)
         return answer
+        
+        
+def projection_operator(u,v):
+    constant=u.inner(v)/u.norm()
+    return u.scalar(constant)
+    
+def orthonormal(u):
+    x=u.norm()
+    y=1/x
+    return u.scalar(y)
+
+def GrammSchmidt(lijst):
+    aantal=len(lijst)
+    u_1=orthonormal(lijst[0])
+    orthogonaal=[u_1]
+    for i in range(2,aantal+1):
+        u_i=lijst[i-1]
+        for j in range(1,i):
+            u_i = u_i.lincomb(projection_operator(orthogonaal[j-1],lijst[i-1]), 1, -1)
+            u_i=orthonormal(u_i)
+    orthogonaal.append(u_i)
+    #print(orthogonaal)
+    return(orthogonaal)
+        
+
+        
+        
+            
+        
+            
+        
+    
         
         
         
--- Opdracht5/test_vector.py
+++ Opdracht5/test_vector.py
@@ -1,5 +1,6 @@
 from Vector import Vector
-
+a=Vector(3)
+print(a)
 u = Vector(3,[1,2,3])
 v = Vector(3, 3.5)
 w=u.lincomb(v,10,1)
@@ -8,5 +9,12 @@ print(w)
 print(w.norm())
 print(w.inner(u))
 
+from Vector import GrammSchmidt
+v0=Vector(2,[3,1])
+v1=Vector(2,[2,2])
+W=GrammSchmidt([v0,v1])
+print(W[0])
+print(W[1])
 
+print(W[0].norm())
 
